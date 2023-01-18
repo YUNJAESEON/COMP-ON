c4e1c2e07086ae198522fd520610a13e251f8c2e
YUNJAESEON <106030257+YUNJAESEON@users.noreply.github.com>
2023-01-18 17:48:35 +0900

    기수 선택 메뉴 (hover)
    기수 선택 메뉴 (hover)

--- project/pro.js
+++ project/pro.js
@@ -1,44 +1,10 @@
-window.onload=()=>{
-  document.querySelector('.dropbtn_click').onclick = ()=>{
-    dropdown();
-  }
-  document.getElementsByClassName('fastfood').onclick = ()=>{
-    showMenu(value);
-  };
-  dropdown = () => {
-    var v = document.querySelector('.dropdown-content');
-    var dropbtn = document.querySelector('.dropbtn')
-    v.classList.toggle('show');
-    dropbtn.style.borderColor = 'rgb(94, 94, 94)';
-  }
+function dp_menu(){
+  let click = document.getElementById("dropdown-submenu");
+  if(click.style.display === "none"){
+      click.style.display = "block";
 
-  showMenu=(value)=>{
-    var dropbtn_icon = document.querySelector('.dropbtn_icon');
-    var dropbtn_content = document.querySelector('.dropbtn_content');
-    var dropbtn_click = document.querySelector('.dropbtn_click');
-    var dropbtn = document.querySelector('.dropbtn');
+  }else{
+      click.style.display = "none";
 
-    dropbtn_icon.innerText = '';
-    dropbtn_content.innerText = value;
-    dropbtn_content.style.color = '#252525';
-    dropbtn.style.borderColor = '#3992a8';
-  }
-}
-window.onclick= (e)=>{
-  if(!e.target.matches('.dropbtn_click')){
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-
-    var dropbtn_icon = document.querySelector('.dropbtn_icon');
-    var dropbtn_content = document.querySelector('.dropbtn_content');
-    var dropbtn_click = document.querySelector('.dropbtn_click');
-    var dropbtn = document.querySelector('.dropbtn');
-
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
   }
 }
